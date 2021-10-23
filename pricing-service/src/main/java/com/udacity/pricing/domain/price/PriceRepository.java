ca0d2554106178a493b1a035fe03a8984b645897
rizwanr <rizwanrenesa@gmail.com>
2021-10-23 15:48:27 +0600

    build out a microservice that provides information about  pricing

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -1,8 +1,13 @@
 package com.udacity.pricing.domain.price;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
+//creating a repository that extends CrudRepository
 @Repository
-public class PriceRepository {
+public interface PriceRepository extends CrudRepository<Price, Long> {
+
+
 
 }
